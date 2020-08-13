c194ccf3ab87388078208dbf8eef75ca762b320e
abhay316-prog <69642819+abhay316-prog@users.noreply.github.com>
2020-08-13 11:57:20 -0500

    Add files via upload

---
+++ codename.js
@@ -0,0 +1,240 @@
+ var wordsArray = ["Acne","Addendum","Advertise","Aircraft","Aisle","Alligator","Alphabetize","America","Ankle","Apathy","Applause","Application","Archaeologist","Aristocrat","Arm","Asleep","Astronaut","Athlete","Atlantis","Aunt","Avocado","Baby-Sitter","Backbone","Bag","Baguette","Bald","Balloon","Banana","Banister","Baseball","Baseboards","Basketball","Bat","Battery","Beach","Beanstalk","Bedbug","Beer","Beethoven","Belt","Bib","Bicycle","Big","Bike","Billboard","Bird","Birthday","Bite","Blacksmith","Blanket","Bleach","Blossom","Blueprint","Blunt","Blur","Boa","Boat","Bob","Bobsled","Body","Bomb","Bonnet","Book","Booth","Bowtie","Box","Boy","Brainstorm","Brand","Brave","Bride","Bridge","Broccoli","Broken","Broom","Bruise","Brunette","Bubble","Buddy","Buffalo","Bulb","Bunny","Bus","Buy","Cabin","Cafeteria","Cake","Calculator","Campsite","Can","Canada","Candle","Candy","Cape","Capitalism","Car","Cardboard","Cartography","Cat","Cd","Ceiling","Cell","Century","Chair","Chalk","Champion","Charger","Cheerleader","Chef","Chess","Chew","Chicken","Chime","China","Chocolate","Church","Circus","Clay","Cliff","Cloak","Clown","Clue","Coach","Coal","Coaster","Cog","Cold","College","Comfort","Computer","Cone","Constructor","Continuum","Conversation","Cook","Coop","Cord","Corduroy","Cot","Cough","Cow","Cowboy","Crayon","Cream","Crisp","Criticize","Crow","Cruise","Crumb","Crust","Cuff","Curtain","Cuticle","Czar","Dad","Dart","Dawn","Day","Deep","Defect","Dent","Dentist","Desk","Dictionary","Dimple","Dirty","Ditch","Diver","Doctor","Dog","Doghouse","Doll","Dominoes","Door","Dot","Drain","Draw","Dream","Dress","Drink","Drip","Drums","Dryer","Duck","Dump","Dunk","Dust","Ear","Eat","Ebony","Elbow","Electricity","Elephant","Elevator","Elf","Elm","Engine","England","Escalator","Eureka","Europe","Evolution","Extension","Eyebrow","Fan","Fancy","Fast","Feast","Fence","Fiddle","Figment","Finger","Fire","First","Fishing","Fix","Fizz","Flagpole","Flannel","Flashlight","Flock","Flotsam","Flower","Flu","Flush","Flutter","Fog","Foil","Football","Forehead","Forever","Fortnight","France","Freckle","Freight","Fringe","Frog","Frown","Gallop","Game","Garbage","Garden","Gasoline","Gem","Ginger","Gingerbread","Girl","Glasses","Goblin","Gold","Goodbye","Grandpa","Grape","Grass","Gratitude","Grey","Green","Guitar","Gum","Gumball","Hair","Half","Handle","Handwriting","Hang","Happy","Hat","Hatch","Headache","Heart","Hedge","Helicopter","Hem","Hide","Hill","Hockey","Homework","Honk","Horse","Hose","Hot","House","Houseboat","Hug","Humidifier","Hungry","Hurdle","Hurt","Hut","Ice","Implode","Inn","Inquisition","Intern","Internet","Invitation","Ironic","Ivory","Ivy","Jade","Japan","Jeans","Jelly","Jet","Jig","Jog","Journal","Jump","Key","Killer","Kilogram","King","Kitchen","Kite","Knee","Kneel","Knife","Knight","Koala","Lace","Ladder","Ladybug","Lag","Landfill","Lap","Laugh","Laundry","Law","Lawn","Lawnmower","Leak","Leg","Letter","Level","Lifestyle","Ligament","Light","Lightsaber","Lime","Lion","Lizard","Log","Loiterer","Lollipop","Loveseat","Loyalty","Lunch","Lunchbox","Lyrics","Machine","Macho","Mailbox","Mammoth","Mark","Mars","Mascot","Mast","Matchstick","Mate","Mattress","Mess","Mexico","Mine","Mistake","Modern","Mold","Mom","Monday","Money","Monitor","Monster","Moon","Mop","Moth","Motorcycle","Mountain","Mouse","Mower","Mud","Music","Mute","Nature","Negotiate","Neighbor","Nest","Neutron","Niece","Night","Nightmare","Nose","Oar","Observatory","Office","Oil","Old","Olympian","Opaque","Opener","Orbit","Organ","Organize","Outer","Outside","Overture","Paint","Pajamas","Palace","Pants","Paper","Paper","Park","Parody","Party","Password","Pastry","Pawn","Pear","Pen","Pencil","Pendulum","Penny","Pepper","Personal","Philosopher","Phone","Photograph","Piano","Picnic","Pigpen","Pillow","Pilot","Pinch","Ping","Pirate","Plaid","Plan","Plank","Plate","Platypus","Playground","Plow","Plumber","Pocket","Poem","Point","Pole","Pomp","Pong","Pool","Popsicle","Population","Portfolio","Positive","Post","Princess","Procrastinate","Protestant","Psychologist","Publisher","Punk","Puppet","Puppy","Push","Puzzle","Quarantine","Queen","Quicksand","Quiet","Race","Radio","Raft","Rag","Rainbow","Rainwater","Random","Ray","Recycle","Red","Regret","Reimbursement","Retaliate","Rib","Riddle","Rim","Rink","Roller","Room","Rose","Round","Roundabout","Rung","Runt","Rut","Sad","Safe","Salmon","Salt","Sandbox","Sandcastle","Sandwich","Sash","Satellite","Scar","Scared","School","Scramble","Scuff","Seashell","Season","Sentence","Set","Shaft","Shallow","Shampoo","Shark","Sheep","Sheets","Sheriff","Shipwreck","Shirt","Shoelace","Short","Shower","Shrink","Sick","Siesta","Silhouette","Singer","Sip","Skate","Skating","Ski","Slam","Sleep","Sling","Slow","Slump","Smith","Sneeze","Snow","Snuggle","Song","Space","Spare","Speakers","Spider","Spit","Sponge","Spool","Spoon","Spring","Sprinkler","Spy","Square","Squint","Stairs","Standing","Star","State","Stick","Stockholder","Stoplight","Stout","Stove","Straw","Stream","Streamline","Stripe","Student","Sun","Sunburn","Sushi","Swamp","Swarm","Sweater","Swimming","Swing","Talk","Taxi","Teacher","Teapot","Teenager","Telephone","Ten","Tennis","Thief","Think","Throne","Thunder","Tide","Tiger","Time","Tinting","Tiptoe","Tiptop","Tired","Tissue","Toast","Toilet","Tool","Toothbrush","Tornado","Tournament","Tractor","Train","Trash","Treasure","Tree","Triangle","Trip","Truck","Tub","Tutor","Television","Twig","Type","Unemployed","Upgrade","Vest","Vision","Wag","Water","Watermelon","Wax","Wedding","Weed","Welder","Whatever","Wheelchair","Whiplash","Whisk","Whistle","White","Wig","Will","Windmill","Winter","Wish","Wolf","Wool","World","Worm","Wristwatch","Yardstick","Zen","Zero","Zipper","Zone","Zoo","africa","agent","air","alien","amazon","angel","antarctica","apple","arm","back","band","bank","bark","beach","belt","berlin","berry","board","bond","boom","bow","box","bug","canada","capital","cell","center","china","chocolate","circle","club","compound","copper","crash","cricket","cross","death","dice","dinosaur","doctor","dog","dress","dwarf","eagle","egypt","engine","england","europe","eye","fair","fall","fan","field","file","film","fish","flute","fly","forest","fork","france","gas","ghost","giant","glass","glove","gold","grass","greece","green","ham","head","himalaya","hole","hood","hook","human","horseshoe","hospital","hotel","ice","icecream","india","iron","ivory","jam","jet","jupiter","kangaroo","ketchup","kid","king","kiwi","knife","knight","lab","lap","laser","lawyer","lead","lemon","limousine","log","mammoth","maple","march","mass","mercury","millionaire","model","mole","moscow","mouth","mug","needle","net","new york","night","note","novel","nurse","nut","oil","olive","olympus","opera","orange","paper","park","part","paste","phoenix","piano","telescope","teacher","switch","swing","sub","stick","staff","stadium","sprint","spike","snowman","slip","shot","shadow","server","ruler","row","rose","root","rome","rock","robot","robin","revolution","rat","racket","queen","press","port","pilot","time","tooth","tower","truck","triangle","trip","turkey","undertaker","unicorn","vacuum","van","wake","wall","war","washer","washington","water","wave","well","whale","whip","worm","yard"];
+	
+ var innerHTML = "";	
+
+    function createGrid(){	
+		if(!innerHTML)
+			innerHTML = $(".container").html();
+		
+		$(".container").html(innerHTML);
+		var cell_count = 0;
+		var cell_data = new Set();        
+		firstColor = "#F00";
+		firstClass = "first-color";
+		
+		secondColor = "#00F";	
+		secondClass = "second-color";	
+		
+		if (Math.round(Math.random())) { 
+			firstColor = "#00F";
+			firstClass = "second-color";
+			
+			secondColor = "#F00";
+			secondClass = "first-color";
+		}
+		$("#tablegrid").addClass("default-color");
+		//$(`td`).css("background-color:","#FFF")			
+	
+		firstTeam = {name:"",color:"",words:[],score:9,turn:true};
+		secondTeam = {name:"",color:"",words:[],score:8};
+		neutral = {name:"",words:[]};
+		
+		// set the first and second team based on the start color.
+		if (firstColor == "#F00"){
+			firstTeam.name = "RED's";
+			secondTeam.name = "BLUE's";
+		}
+		else{
+			firstTeam.name = "BLUE's";
+			secondTeam.name = "RED's";
+		}
+			
+		while (firstTeam.words.length < 9){
+			var newWord = wordsArray[Math.floor(Math.random()*wordsArray.length)];		
+			firstTeam.words.push(newWord.toUpperCase());
+			removeItem(wordsArray,newWord);	
+		}
+		
+		while (secondTeam.words.length < 8){
+			var newWord = wordsArray[Math.floor(Math.random()*wordsArray.length)];		
+			secondTeam.words.push(newWord.toUpperCase());
+			removeItem(wordsArray,newWord);	
+		}
+		
+		while (neutral.words.length < 7){
+			var newWord = wordsArray[Math.floor(Math.random()*wordsArray.length)];	
+			neutral.words.push(newWord.toUpperCase());	
+		}
+		
+		var assassign = wordsArray[Math.floor(Math.random()*wordsArray.length)];
+		
+        while (cell_count < 25 ){
+			var randomArr = [];
+            var random = Math.floor(Math.random() * (25)) + 1; 
+			
+            if (!cell_data.has(random)){
+                if (cell_count < 8){
+            //        $(`#${random}`).css("background-color",""+secondColor+"");	
+					$(`#${random}`).addClass(secondClass);
+					$(`#${random}`).text(secondTeam.words[cell_count]); 
+					$(`#${random}`).attr("team",secondColor);
+					
+                }
+                else if (cell_count < 17){
+            //        $(`#${random}`).css("background-color",""+firstColor+"");
+					$(`#${random}`).addClass(firstClass);
+					$(`#${random}`).text(firstTeam.words[cell_count-8]);
+					$(`#${random}`).attr("team",firstColor);
+                }
+				else if (cell_count < 24){
+                  //  $(`#${random}`).css("background-color",""+firstColor+"");
+				    $(`#${random}`).addClass("neutral-color");
+					$(`#${random}`).text(neutral.words[cell_count-17]);
+					$(`#${random}`).attr("team","neutral-color");
+                }
+                else {
+             //       $(`#${random}`).css("background-color","grey");
+					$(`#${random}`).addClass("assassign-color");
+					$(`#${random}`).text(assassign.toUpperCase());
+					$(`#${random}`).attr("team","assassign-color");
+                }
+                cell_data.add(random)
+                cell_count++;
+            }
+        }	
+			
+		// Set the border based on which team starts.
+		$("#tablegrid").css("border-color",""+firstColor+"");
+	/*	$("#3").css("border-top-color",""+firstColor+"");
+		$("#11").css("border-left-color",""+firstColor+"");
+		$("#15").css("border-right-color",""+firstColor+"");
+		$("#23")css("border-bottom-color",""+firstColor+"");
+*/
+		// Set whos turn it is.
+		$("#teamturn").text(firstTeam.name + " Turn");
+		$("#endturn").text("End " + firstTeam.name + " Turn");
+		$("#teamturn").addClass(firstClass);
+		updateScore();
+    }
+	
+	$(document).on('click','td', function() { 	
+		var team = $(this).attr("team");
+		if (team == firstColor){
+			$(this).css("background-color",""+firstColor+"");
+			//$(this).addClass("first-color");
+			$(this).removeClass("default-color");
+			firstTeam.score--;
+			updateScore();
+			if(!firstTeam.turn)				
+				endTurn();
+			
+		}
+		else if (team == secondColor){
+			$(this).css("background-color",""+secondColor+"");
+			//$(this).addClass("second-color");
+			$(this).removeClass("default-color");
+			secondTeam.score--;
+			updateScore();
+			if(firstTeam.turn)			    
+				endTurn();
+			
+			
+		}
+		else if	(team == "neutral-color"){
+			$(this).css("background-color","#B0ADAD");
+			//$(this).addClass("neutral-color");
+			$(this).removeClass("default-color");
+			// If neutral is hit then end turn
+			endTurn();
+		}
+		else {
+			$(this).css("background-color","#7CFC00");
+			//$(this).addClass("assassign-color");
+			
+			// game over
+			if(firstTeam.turn)
+				$(".data-row").html("<div class='center "+secondClass+"'>"+secondTeam.name+" Win !!</div>");				
+			else 
+				$(".data-row").html("<div class='center "+firstClass+"'>"+firstTeam.name+" Win !!</div>");
+			
+			
+			// Change to spy view to see all colors
+			if (!$( "#tablegrid" ).hasClass( "spy" ))
+				changeView();			
+			
+		}	
+		
+		$(this).removeClass("default-color");
+		$(this).css("color","#FFF");
+		$(this).css("font-weight","bold");
+		
+
+	});
+
+
+function removeItem(array, item){
+    for(var i in array){
+        if(array[i]==item){
+            array.splice(i,1);
+            break;
+        }
+    }
+}
+
+function changeView(){
+	if ($( "#tablegrid" ).hasClass( "spy" )){
+		$( "#tablegrid" ).removeClass( "spy" );
+		$( "#tablegrid" ).addClass( "default-color" );		
+		$("#toggle-view").text("Change to Spy Master");
+		$("#view-type").text("Player View");
+	} else {
+		$( "#tablegrid" ).addClass( "spy" );
+		$( "#tablegrid" ).removeClass( "default-color" );
+		$("#toggle-view").text("Change to Player");
+		$("#view-type").text("Spy Master View");
+		
+	}	
+}
+
+function endTurn(){
+	$("#teamturn").removeClass();
+	if ($("#endturn").text()== "End " + firstTeam.name + " Turn") {
+		$("#endturn").text("End " + secondTeam.name + " Turn");		
+	    $("#teamturn").text(secondTeam.name + " Turn");
+		$("#teamturn").addClass(secondClass)
+		$("#teamturn").addClass("team-turn");
+		
+	}	
+	else {
+		$("#endturn").text("End " + firstTeam.name + " Turn");
+		$("#teamturn").text(firstTeam.name + " Turn");
+		$("#teamturn").addClass(firstClass,"team-turn");
+		$("#teamturn").addClass("team-turn");
+		
+	}	
+	firstTeam.turn = !firstTeam.turn;
+}
+
+
+
+
+
+function updateScore () { 
+	$("#team1score").text(firstTeam.score).css("color",""+firstColor+"");
+	$("#team2score").text(secondTeam.score).css("color",""+secondColor+"");
+	// team wins and show spy view
+	if(!firstTeam.score){				
+		$(".data-row").html("<div class='center "+firstClass+"'>"+firstTeam.name+" Win !!</div>");		
+		changeView();		
+	} else if(!secondTeam.score){
+		$(".data-row").html("<div class='center "+secondClass+"'>"+secondTeam.name+" Win !!</div>");
+		changeView();	
+	}	
+}	
+
+function printKey () {
+	var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+    mywindow.document.write('<html><head><title>SPy Master</title>');
+    mywindow.document.write('</head><style>table { width: 100%;}td {  width:10%;height:70px;  border: 5px solid black;  position: relative;  text-align: center;  font-weight:bold;}div {font-weight:bold;padding-left:20px;}.container{width:40%;float:left;}.grid {border-width:15px;border-style: solid; margin:20px;}.button{margin-left:20px;}.first-color{background-color:#F00;}.second-color{background-color:#00F;}.default-color td {background-color:#FFF;}.neutral-color {background-color:#B0ADAD;}.assassign-color {background-color:#7CFC00;}.end-turn{float:right;}.team-turn{text-align:center;margin-left:35%;color:#FFF;font-weight:bold;padding: 5px;}.data-row{margin-right:-20px;}.center{text-align:center;color:#FFF;font-weight:bold;padding: 5px;}.rules {margin-left:50%;width:40%;}</style><body><table>');    mywindow.document.write('<h1>Spy Master</h1>');
+    mywindow.document.write($('table').html());
+    mywindow.document.write('<table></body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+   // mywindow.print();
+   // mywindow.close();
+   return true;
+}	
+	
